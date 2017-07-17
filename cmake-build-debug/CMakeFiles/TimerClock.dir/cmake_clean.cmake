10812c94a73c6d7031587e1165bfe6b0433fa770
Francesco <todinofrancesco97@gmail.com>
2017-07-17 10:14:37 +0200

    Hello git

---
+++ cmake-build-debug/CMakeFiles/TimerClock.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TimerClock.dir/main.cpp.o"
+  "TimerClock.pdb"
+  "TimerClock"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TimerClock.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
